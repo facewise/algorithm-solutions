3b83225e19a5ccff3de0cedad951f34a1f1c279d
Yonghyun Kim <62998666+facewise@users.noreply.github.com>
2024-01-04 18:24:53 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 868 ms, Memory: 298512 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.kt
@@ -0,0 +1,15 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val bw = BufferedWriter(OutputStreamWriter(System.out))
+    repeat(br.readLine().toInt()) {
+        val sum = br.readLine().split(" ").sumOf { it.toInt() }
+        bw.write("$sum\n")
+    }
+    bw.flush()
+    bw.close()
+}
