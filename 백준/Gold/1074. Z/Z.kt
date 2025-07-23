575fda291927489b20aad5ca3d8a4d1636242b03
facewise <dydgus.k@gmail.com>
2025-07-23 12:06:13 +0900

    [Gold V] Title: Z, Time: 112 ms, Memory: 21448 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.kt
@@ -0,0 +1,31 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+
+fun main() {
+    val reader = BufferedReader(InputStreamReader(System.`in`))
+    val writer = BufferedWriter(OutputStreamWriter(System.out))
+
+    var (n, r, c) = reader.readLine()?.trim()?.split(" ")?.map(String::toInt) ?: return
+
+    var answer = 0
+    var size = 1.shl(n)
+
+    while (size > 1) {
+        val half = size.shr(1)
+        val adder = 1.shl(2 * n - 2)
+        if (r >= half) {
+            answer += adder.shl(1)
+            r -= half
+        }
+        if (c >= half) {
+            answer += adder
+            c -= half
+        }
+        size = 1.shl(--n)
+    }
+
+    writer.write("$answer\n")
+    writer.flush()
+}
